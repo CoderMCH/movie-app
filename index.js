fcec5bc714527e6974e6d0f681c8e8de6004f678
CoderMCH <160289936+CoderMCH@users.noreply.github.com>
2024-03-20 17:28:07 -0400

    reorder cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 const mongo = require("./public/js/mongoDB.js");
 const { check, validationResult } = require("express-validator");
@@ -13,19 +14,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 let auth = require('./public/js/auth.js')(app);
 let passport = require("passport");
 
-const cors = require("cors");
+
 let allowedOrigins = ["http://localhost:1234", "http://localhost:3000"];
-// app.use(cors());
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if(!origin) return callback(null, true);
+//     if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+//       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+//       return callback(new Error(message ), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
 
 app.use(express.static("public"));
 
