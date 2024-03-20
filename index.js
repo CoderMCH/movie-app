1c8c20d10f86a88bfe89d13b889032e4e72ddde4
CoderMCH <160289936+CoderMCH@users.noreply.github.com>
2024-03-20 17:19:34 -0400

    enable cors

--- index.js
+++ index.js
@@ -14,18 +14,18 @@ let auth = require('./public/js/auth.js')(app);
 let passport = require("passport");
 
 const cors = require("cors");
-let allowedOrigins = ["*"];
-app.use(cors());
-// app.use(cors({
-//   origin: (origin, callback) => {
-//     if(!origin) return callback(null, true);
-//     if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-//       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-//       return callback(new Error(message ), false);
-//     }
-//     return callback(null, true);
-//   }
-// }));
+let allowedOrigins = ["http://localhost:1234", "http://localhost:3000"];
+// app.use(cors());
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 app.use(express.static("public"));
 
