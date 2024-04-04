ef30f15d237f0bf2abb2ac212d036a0dcfc6dc7d
CoderMCH <160289936+CoderMCH@users.noreply.github.com>
2024-04-04 15:33:04 -0400

    enable CORS

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
-app.use(cors());
 const mongo = require("./public/js/mongoDB.js");
 const { check, validationResult } = require("express-validator");
 
@@ -16,18 +15,17 @@ let auth = require('./public/js/auth.js')(app);
 let passport = require("passport");
 
 
-let allowedOrigins = ["http://localhost:1234", "http://localhost:3000"];
-
-// app.use(cors({
-//   origin: (origin, callback) => {
-//     if(!origin) return callback(null, true);
-//     if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-//       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-//       return callback(new Error(message ), false);
-//     }
-//     return callback(null, true);
-//   }
-// }));
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://main--mch-flix.netlify.app'];
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 app.use(express.static("public"));
 
